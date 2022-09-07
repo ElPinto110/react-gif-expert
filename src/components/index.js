b06878329654e36a95f56746ce869155528e5ddf
Victor <elpinto110@gmail.com>
2022-09-07 10:30:48 -0400

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+//Archivo barril: se usa para almacenar todas las 
+//exportaciones, deesta manera se reduce el codigo
+//al llamar componentes, helpers y hooks.
+
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifItem';
